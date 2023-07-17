6ce5960d8bc723a80a6595d3d16d7f93d7aa6ed6
David Barrera <7396davidrbh@gmail.com>
2023-07-16 20:11:48 -0400

    se agrego la funcion sessionStart

--- Controllers/Error.php
+++ Controllers/Error.php
@@ -3,7 +3,12 @@
 	class Errors extends Controllers{
 		public function __construct()
 		{
+			//sessionStart();
 			parent::__construct();
+			if(empty($_SESSION['login']))
+			{
+				header('location:' .base_url() .'/login');
+			}
 		}
 
 		public function notFound()
