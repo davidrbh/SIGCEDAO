35983d0bac2bc20ebf82f2b84fdb6b72ec7d6996
David Barrera <7396davidrbh@gmail.com>
2023-07-16 20:17:35 -0400

    se agrego la funcion sessionStart

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -13,8 +13,12 @@ class Usuarios extends Controllers
 
 	public function __construct()
 	{
-
+		sessionStart();
 		parent::__construct();
+		if(empty($_SESSION['login']))
+			{
+				header('location:' .base_url() .'/login');
+			}
 
 
 	}
