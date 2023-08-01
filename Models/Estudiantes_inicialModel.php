0178b9685a0e6083bcec5e14504c10218c414094
David Barrera <7396davidrbh@gmail.com>
2023-08-01 19:56:28 -0400

    se agregaron nuevas funciones

--- Models/Estudiantes_inicialModel.php
+++ Models/Estudiantes_inicialModel.php
@@ -16,5 +16,28 @@ class Estudiantes_inicialModel extends Mysql
 			return $request;
 		}
 
+        public function selectTipoSecciones_inicial(int $turno)
+        {
+            $this->intTurno = $turno;
+            $sql ="SELECT tipo_seccion.id_tipo_seccion, tipo_seccion.nombre_seccion
+                   FROM `tipo_seccion` 
+                   WHERE turno_id = $this->intTurno AND `id_tipo_seccion` >14 OR `id_tipo_seccion` = 1";
+			$request = $this->select_all($sql);
+			return $request;
+
+        }
+
+        public function selectNombreDocente($seccion)
+		{
+			$this->intSeccion = $seccion;
+			$sql = "SELECT `tipo_seccion`.`id_tipo_seccion`, `tipo_seccion`.`docente_id`, `docentes`.`nombre_docente`
+                    FROM `tipo_seccion` 
+                    LEFT JOIN `docentes` 
+                    ON `tipo_seccion`.`docente_id` = `docentes`.`id_docentes` 
+                    WHERE `tipo_seccion`.`id_tipo_seccion` = $this->intSeccion";
+			$request = $this->select_all($sql);
+			return $request;
+		}
+
 }
 ?>
