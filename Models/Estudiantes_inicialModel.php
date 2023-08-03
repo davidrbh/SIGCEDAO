d42c6c7f7107e9e324b5c68c75ebb276a079da99
David Barrera <7396davidrbh@gmail.com>
2023-08-03 11:26:23 -0400

    se modifico el query de consulta de la funcion getSelectSecciones_inicial y getNombreDocente_inicial

--- Models/Estudiantes_inicialModel.php
+++ Models/Estudiantes_inicialModel.php
@@ -16,26 +16,32 @@ class Estudiantes_inicialModel extends Mysql
 			return $request;
 		}
 
-        public function selectTipoSecciones_inicial(int $turno)
+        public function selectSecciones_inicial(int $turno)
         {
             $this->intTurno = $turno;
-            $sql ="SELECT tipo_seccion.id_tipo_seccion, tipo_seccion.nombre_seccion
-                   FROM `tipo_seccion` 
-                   WHERE turno_id = $this->intTurno AND `id_tipo_seccion` >14 OR `id_tipo_seccion` = 1";
+            $sql ="SELECT `seccion`.`id_seccion`,`seccion`.`docente_id`,`seccion`.`desc_seccion_id`,`desc_seccion`.`nombre_seccion`,`seccion`.`periodo_escolar`,`desc_seccion`.`turno_id`
+                   FROM `seccion`
+                   JOIN `desc_seccion` 
+                   ON   `desc_seccion`.`id_desc_seccion` = `seccion`.`desc_seccion_id`
+                   AND  `desc_seccion`.`id_desc_seccion` > 14 
+                   AND  `desc_seccion`.`turno_id` = $this->intTurno
+                   WHERE`seccion`.`periodo_escolar` = '2023-2024' OR '1'
+                   AND  `seccion`.`status` = 1  
+                   ORDER BY `seccion`.`desc_seccion_id`";
 			$request = $this->select_all($sql);
 			return $request;
 
         }
 
         
-        public function selectNombreDocente($seccion)
+        public function selectNombreDocente_inicial($seccion)
 		{
 			$this->intSeccion = $seccion;
 			$sql = "SELECT `seccion`.`id_seccion`, `docentes`.`nombre_docente`
                     FROM `seccion`
-                    JOIN `docentes` 
-                    ON   `docentes`.`id_docentes` = `seccion`.`id_seccion`
-                    WHERE`seccion`.`id_seccion` =  $this->intSeccion";
+                    JOIN `docentes`
+                    ON docentes.id_docentes = seccion.docente_id
+                    WHERE`seccion`.`id_seccion` =   $this->intSeccion";
 			$request = $this->select_all($sql);
 			return $request;
 		}
