838ecb7c091629d537a6488ea299374822f209e9
David Barrera <7396davidrbh@gmail.com>
2023-08-09 19:08:30 -0400

    se cambio el modulo de representantes por el de configuracion de secciones

---
+++ Models/Secciones_configModel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Clase que hace referencia a Secciones_configModel.php
+ */
+class Secciones_configModel extends Mysql
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+?>
