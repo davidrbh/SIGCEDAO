df4c50ce1d860f0556c03bd58b52a72937182252
David Barrera <7396davidrbh@gmail.com>
2023-09-09 23:32:48 -0400

    se agrego todas la tablas de los alumno hacia la vista,controlador y modelos

--- Controllers/Estudiantes.php
+++ Controllers/Estudiantes.php
@@ -130,7 +130,196 @@ class Estudiantes extends Controllers
 			}else{
 				$arrData[$i]['turno'] ='Vespertino' ;
 			}
+			if ($arrData[$i]['genero']== 1){
+				$arrData[$i]['genero'] = 'Masculino';
+			}else{
+				$arrData[$i]['genero'] = 'Femenino';
+			}if ($arrData[$i]['nacionalidad_alumno']== 1){
+				$arrData[$i]['nacionalidad_alumno'] = 'Venezolana';
+			}else{
+				$arrData[$i]['nacionalidad_alumno'] = 'Extranjera';
+			}if ($arrData[$i]['repitiente']== 1){
+				$arrData[$i]['repitiente'] = 'Si';
+			}else{
+				$arrData[$i]['repitiente'] = 'No';
+			}if ($arrData[$i]['enfermedad_padece']== 1){
+				$arrData[$i]['enfermedad_padece'] = 'Si';
+			}else{
+				$arrData[$i]['enfermedad_padece'] = 'No';
+			}if ($arrData[$i]['enfermedad_1']== ""){
+				$arrData[$i]['enfermedad_1'] = 'No padece de enfermedad';
+			}else{
+				$arrData[$i]['enfermedad_1'] == $arrData[$i]['enfermedad_1'];
+			}if ($arrData[$i]['tratamiento_enfermedad']== 1){
+				$arrData[$i]['tratamiento_enfermedad'] = 'Si';
+			}else{
+				$arrData[$i]['tratamiento_enfermedad'] = 'No';
+			}if ($arrData[$i]['tratamiento_1']== ""){
+				$arrData[$i]['tratamiento_1'] = 'Ninguno';
+			}else{
+				$arrData[$i]['tratamiento_1'] == $arrData[$i]['tratamiento_1'];
+			}if ($arrData[$i]['alergico_medicamento']== 1){
+				$arrData[$i]['alergico_medicamento'] = 'Si';
+			}else{
+				$arrData[$i]['alergico_medicamento'] = 'No';
+			}if ($arrData[$i]['alergico_1']== ""){
+				$arrData[$i]['alergico_1'] = 'Ninguno';
+			}else{
+				$arrData[$i]['alergico_1'] == $arrData[$i]['alergico_1'] ;
+			}if ($arrData[$i]['rubeola']== 1){
+				$arrData[$i]['rubeola'] = 'Si padecio';
+			}else{
+				$arrData[$i]['rubeola'] = 'No padecio' ;
+			}if ($arrData[$i]['sarampion']== 1){
+				$arrData[$i]['sarampion'] = 'Si padecio';
+			}else{
+				$arrData[$i]['sarampion'] = 'No padecio' ;
+			}if ($arrData[$i]['lechina']== 1){
+				$arrData[$i]['lechina'] = 'Si padecio';
+			}else{
+				$arrData[$i]['lechina'] ='No padecio';
+			}if ($arrData[$i]['paperas']== 1){
+				$arrData[$i]['paperas'] = 'Si padecio';
+			}else{
+				$arrData[$i]['paperas'] ='No padecio' ;
+			}if ($arrData[$i]['otra_enfermedad']== 1){
+				$arrData[$i]['otra_enfermedad'] = 'Si';
+			}else{
+				$arrData[$i]['otra_enfermedad'] ='No' ;
+			}if ($arrData[$i]['ninguna_enfermedad']== 1){
+				$arrData[$i]['ninguna_enfermedad'] = 'Ninguna enfermedad';
+			}else{
+				$arrData[$i]['ninguna_enfermedad'] ='Estuvo enfermo' ;
+			}if ($arrData[$i]['psicologo']== 1){
+				$arrData[$i]['psicologo'] = 'Si';
+			}else{
+				$arrData[$i]['psicologo'] ='No' ;
+			}
 
+			if ($arrData[$i]['alumno_seva_solo']== 1){
+				$arrData[$i]['alumno_seva_solo'] = 'Si';
+			}else{
+				$arrData[$i]['alumno_seva_solo'] ='No' ;
+			}
+
+			if ($arrData[$i]['vive_madre']== 1){
+				$arrData[$i]['vive_madre'] = 'Si';
+			}else{
+				$arrData[$i]['vive_madre'] ='No' ;
+			}
+
+			if ($arrData[$i]['vive_padre']== 1){
+				$arrData[$i]['vive_padre'] = 'Si';
+			}else{
+				$arrData[$i]['vive_padre'] ='No' ;
+			}
+
+			if ($arrData[$i]['vive_hermanos']== 1){
+				$arrData[$i]['vive_hermanos'] = 'Si';
+			}else{
+				$arrData[$i]['vive_hermanos'] ='No' ;
+			}
+
+			if ($arrData[$i]['vive_otra_persona']== 1){
+				$arrData[$i]['vive_otra_persona'] = 'Si';
+			}else{
+				$arrData[$i]['vive_otra_persona'] ='No' ;
+			}
+
+			if ($arrData[$i]['hermanos_plantel']== 1){
+				$arrData[$i]['hermanos_plantel'] = 'Si';
+			}else{
+				$arrData[$i]['hermanos_plantel'] ='No' ;
+			}
+
+			if ($arrData[$i]['cuantos_hermanos_plantel']== ""){
+				$arrData[$i]['cuantos_hermanos_plantel'] = 'Ninguno';
+			}else{
+				$arrData[$i]['cuantos_hermanos_plantel'] == $arrData[$i]['cuantos_hermanos_plantel'];
+			}
+
+			if ($arrData[$i]['grado_cursanHermanos']== ""){
+				$arrData[$i]['grado_cursanHermanos'] = 'Ninguno';
+			}else{
+				$arrData[$i]['grado_cursanHermanos'] == $arrData[$i]['grado_cursanHermanos'];
+			}
+
+			if ($arrData[$i]['turno_cursanHermanos']== ""){
+				$arrData[$i]['turno_cursanHermanos'] = 'Ninguno';
+			}else{
+				$arrData[$i]['turno_cursanHermanos'] == $arrData[$i]['turno_cursanHermanos'] ;
+			}
+
+			if ($arrData[$i]['boletin_calificacion']== 1){
+				$arrData[$i]['boletin_calificacion'] = 'Si';
+			}else{
+				$arrData[$i]['boletin_calificacion'] ='No' ;
+			}
+
+			if ($arrData[$i]['boletin_promocion']== 1){
+				$arrData[$i]['boletin_promocion'] = 'Si';
+			}else{
+				$arrData[$i]['boletin_promocion'] ='No' ;
+			}
+
+			if ($arrData[$i]['carta_conducta']== 1){
+				$arrData[$i]['carta_conducta'] = 'Si';
+			}else{
+				$arrData[$i]['carta_conducta'] ='No' ;
+			}
+
+			if ($arrData[$i]['copia_partida_nac']== 1){
+				$arrData[$i]['copia_partida_nac'] = 'Si';
+			}else{
+				$arrData[$i]['copia_partida_nac'] ='No' ;
+			}
+
+			if ($arrData[$i]['copia_cedula_alumno']== 1){
+				$arrData[$i]['copia_cedula_alumno'] = 'Si';
+			}else{
+				$arrData[$i]['copia_cedula_alumno'] ='No' ;
+			}
+
+			if ($arrData[$i]['copia_cedula_repre']== 1){
+				$arrData[$i]['copia_cedula_repre'] = 'Si';
+			}else{
+				$arrData[$i]['copia_cedula_repre'] ='No' ;
+			}
+
+			if ($arrData[$i]['foto_alumno']== 1){
+				$arrData[$i]['foto_alumno'] = 'Si';
+			}else{
+				$arrData[$i]['foto_alumno'] ='No' ;
+			}
+
+			if ($arrData[$i]['foto_repre']== 1){
+				$arrData[$i]['foto_repre'] = 'Si';
+			}else{
+				$arrData[$i]['foto_repre'] ='No' ;
+			}
+
+			if ($arrData[$i]['boletin_grado']== 1){
+				$arrData[$i]['boletin_grado'] = 'Si';
+			}else{
+				$arrData[$i]['boletin_grado'] ='No' ;
+			}
+
+			if ($arrData[$i]['otros_documentos']== 1){
+				$arrData[$i]['otros_documentos'] = 'Si';
+			}else{
+				$arrData[$i]['otros_documentos'] ='No' ;
+			}
+
+			if ($arrData[$i]['documentos_1']== ""){
+				$arrData[$i]['documentos_1'] = 'Ninguno';
+			}else{
+				$arrData[$i]['documentos_1'] == $arrData[$i]['documentos_1'] ;
+			}
+
+		
+
+			
+			
 			$arrData[$i]['nombre_repre'] =   $arrData[$i]['nombre_repre'].' '.$arrData[$i]['apellido_repre'];
 
 
--- Views/Estudiantes/estudiantes.php
+++ Views/Estudiantes/estudiantes.php
@@ -33,7 +33,7 @@
                   <thead>
                     <tr>
                       
-                      <th>Acciones</th>
+                      <!-- <th>Acciones</th>
                       <th>Nombre</th>
                       <th>Apellido</th>
                       <th>Cédula</th>
@@ -42,7 +42,98 @@
                       <th>Celular</th>
                       <th>Sección</th>
                       <th>Turno</th>
+                      <th>Status</th> -->
+                      <th>Acciones</th>
+                      <th>Primer nombre</th>
+                      <th>Segundo nombre</th>
+                      <th>Primer apellido</th>
+                      <th>Segundo apellido</th>
+                      <th>Cédula escolar</th>
+                      <th>fecha nacimiento</th>
+                      <th>Género</th>
+                      <th>Nacionalidad</th>
+                      <th>Estado</th>
+                      <th>Lugar de nacimiento</th>
+                      <th>Dirección</th>
+                      <th>Nombre de la madre</th>
+                      <th>Apellido de la madre</th>
+                      <th>Nacionalidad de la madre</th>
+                      <th>Cédula de la madre</th>
+                      <th>Ocupación de la madre</th>
+                      <th>Teléfono de la madre</th>
+                      <th>Nombre del padre</th>
+                      <th>Apellido del padre</th>
+                      <th>Nacionalidad del padre</th>
+                      <th>Cédula del padre</th>
+                      <th>Ocupación del padre</th>
+                      <th>Teléfono del padre</th>
+                      <th>Nombre del representante</th>
+                      <th>Apellido del representante</th>
+                      <th>Nacionalidad del representante</th>
+                      <th>Cédula del representante</th>
+                      <th>Parentesco del representante</th>
+                      <th>Ocupación del representante</th>
+                      <th>Teléfono del representante</th>
+                      <th>Celular del representante</th>
+                      <th>Dirección del representante</th>
+                      <th>Colaboración con el plantel</th>
+                      <th>Nombre del plantel</th>
+                      <th>Grado cursado</th>
+                      <th>Périodo escolar</th>
+                      <th>Literal</th>
+                      <th>Es repitiente?</th>
+                      <th>Tipo de alumno</th>
+                      <th>Turno</th>
+                      <th>Sección</th>
+                      <th>Nuevo périodo escolar</th>
+                      <th>Fucionario que registra</th>
+                      <th>Fecha de inscripción</th>
+                      <th>Padece de enfermedad?</th>
+                      <th>Nombre de la enfermedad</th>
+                      <th>Utiliza tratamiento?</th>
+                      <th>Nombre del tratamiento</th>
+                      <th>Alergico a medicamento?</th>
+                      <th>Nombre del medicamento</th>
+                      <th>Rubeola</th>
+                      <th>Sarampión</th>
+                      <th>Lechina</th>
+                      <th>Paperas</th>
+                      <th>Posee otra enfermedad?</th>
+                      <th>Nombre de la enfermedad</th>
+                      <th>Ninguna enfermdad</th>
+                      <th>Atendido por psicológo?</th>
+                      <th>Tipo de nacimiento de parto</th>
+                      <th>Se va solo?</th>
+                      <th>Nombre del representante</th>
+                      <th>Parentesco</th>
+                      <th>Vive con la madre</th>
+                      <th>Vive con el padre</th>
+                      <th>Vive con hermanos</th>
+                      <th>Cuantos hermanos</th>
+                      <th>Cuantas hermanas</th>
+                      <th>Vive con otra persona</th>
+                      <th>Con quién vive?</th>
+                      <th>Tiene Hermanos en el plantel?</th>
+                      <th>Cuantos hermanos</th>
+                      <th>Grado que cursan</th>
+                      <th>Turno que cursan</th>
+                      <th>Tipo de vivienda</th>
+                      <th>Condición de la vivienda</th>
+                      <th>Ingresos familiares</th>
+                      <th>N* personas que dependen del ingreso</th>
+                      <th>Boletin de calificaciones</th>
+                      <th>Boletin de promoción</th>
+                      <th>Carta de buena conducta</th>
+                      <th>Copia de partida de nacimiento</th>
+                      <th>Copia de cedula del alumno/a</th>
+                      <th>Foto del alumno</th>
+                      <th>Copia de cedula del representante</th>
+                      <th>Foto del representante</th>
+                      <th>Boletin de grando</th>
+                      <th>Otros documentos</th>
+                      <th>Documentos</th>
                       <th>Status</th>
+                      
                    
 
 
